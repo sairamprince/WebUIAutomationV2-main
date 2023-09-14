6bc2ea02606d9bf2dcc56fdfd9021a40a0be146f
sairamprince <sairamprince33@gmail.com>
2023-09-14 17:19:12 +0530

    edit commit

--- src/main/java/com/rgt/engine/TestDriver.java
+++ src/main/java/com/rgt/engine/TestDriver.java
@@ -99,10 +99,10 @@ public class TestDriver
 	Actions act;
 
 	public final String SCENARIO_SHEET_PATH1 = System.getenv("resources/datafiles/TC_Master.xlsx");
-	public final String SCENARIO_SHEET_PATH = System.getProperty("user.dir")+"/resources/datafiles/"+SCENARIO_SHEET_PATH1;
+	public final String SCENARIO_SHEET_PATH = "/resources/datafiles/"+SCENARIO_SHEET_PATH1;
 	//public final String SCENARIO_SHEET_PATH = System.getenv("BUILD_ID");
 	//public final String SCENARIO_SHEET_PATH = System.getProperty("user.dir")+"/resources/datafiles/TC_Master.xlsx";
-	/////public final String SCENARIO_SHEET_PATH = System.getProperty("testCaseFile");
+	///public final String SCENARIO_SHEET_PATH = System.getProperty("testCaseFile");
 	public final String ExtentReport_Path = System.getProperty("user.dir")+"/resources/reports/WebAutomationReport.html";
 	public final String ExcelReport_Path = System.getProperty("user.dir")+"/resources/reports/ExcelReport.xlsx";
 
